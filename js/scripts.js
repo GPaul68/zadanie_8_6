b140791036009fd78f8278523d02101f4faaad2c
Paweł Galeja <pgaleja@gmail.com>
2017-12-28 19:31:01 +0100

    Add js css index.html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//scripts.js
+
+var a = 3;
+var b = 6;
+var value = (a * a) - (2 * a * b) + (b * b);
+console.log("Value= " + value);
+
+if (value > 0) {
+    console.log("Wynik dodatni")
+}
+else if (value < 0) {
+    console.log("Wynik ujemny")
+}
+else {
+    console.log("Wynik równy 0")
+}
